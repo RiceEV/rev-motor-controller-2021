72747c461600866d771647cf9373167ceece4736
EthanDP <etdpeck@gmail.com>
2021-09-28 11:43:49 -0500

    Add SDK and example code, created src directory

---
+++ pac55xx_example_code_v1.4.0/pac55xx_peripheral_examples/keil_proj/project_build_info_keil.h
@@ -0,0 +1,18 @@
+//============================================================= 
+//===== Auto Generated Project Build File - Do NOT Modify ===== 
+//============================================================= 
+ 
+ 
+// keil PROJECT Library Build-20190909.102501 
+ 
+#ifndef PROJECT_BUILD_INFO_H 
+#define PROJECT_BUILD_INFO_H 
+ 
+#define PROJECT_BUILD_STR   		"20190909.102501" 
+#define PROJECT_BUILD_DATE_STR		"20190909" 
+#define PROJECT_BUILD_DATE_NUM		20190909 
+#define PROJECT_BUILD_TIME_STR		"102501" 
+#define PROJECT_BUILD_TIME_NUM		102501 
+ 
+#endif // PROJECT_BUILD_INFO_H 
+ 
